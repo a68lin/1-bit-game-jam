8daccd8bc01a4205bb09607b706f81caa01cc4cd
Ziming (Harry) Zhu <harry.zhu0325@gmail.com>
2023-08-14 17:30:20 -0700

    camera background color

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,6 +7,14 @@ public class CameraMovement : MonoBehaviour
     public Transform target;
     public Vector3 offset;
 
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.backgroundColor = Color.white;
+    }
+
     private void Update()
     {
         transform.position = target.position + offset;
