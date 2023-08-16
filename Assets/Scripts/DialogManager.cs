a75448d3078143d45f7ab2f00f648ff3542f5737
Alex Lin <a68lin@uwaterloo.ca>
2023-08-16 15:59:49 -0400

    Add parameters: index, position

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
-    public GameObject canvas;
-    //[Header("Appear time")]
-    //[Range(0, 10)]
+    public GameObject[] canvas;
+    [Header("Appear time")]
+    [Range(0, 10)]
     public float appearTime = 3f;
+    [Header("Fade time")]
+    [Range(0, 10)]
     public float fadeTime = 2f;
     private Text message;
     private float timeWhenDisappear = 5f;
+    //private const Vector3 position = new Vector3(0, 70, 0);
 
     private void Start()
     {
@@ -30,20 +33,25 @@ public class DialogManager : MonoBehaviour
         }
     }
 
-    public void ShowDialog(string text, float timeAppear = 3f, float timeFade = 2f)
+    public void ShowDialog(string text, int index = 0, Vector3 position = default(Vector3), float timeAppear = 3f, float timeFade = 2f)
     {
         fadeTime = timeFade;
-        GameObject copy = Instantiate<GameObject>(canvas, transform.position, Quaternion.identity);
+        timeWhenDisappear = Time.time + timeAppear;
+
+        GameObject copy = Instantiate<GameObject>(canvas[index], transform.position, Quaternion.identity);
+
         message = copy.GetComponentInChildren<Text>();
+
         message.text = text;
         message.color = Color.black;
         message.alignment = TextAnchor.UpperCenter;
         message.fontSize = 18;
         message.fontStyle = FontStyle.Bold;
+
         RectTransform rt = message.GetComponent<RectTransform>();
-        rt.anchoredPosition3D = new Vector3(0, 70, 0);
+        rt.anchoredPosition3D = position;
         rt.sizeDelta = new Vector2(1000, 50);
-        timeWhenDisappear = Time.time + timeAppear;
+
         Destroy(copy, timeAppear + timeFade);
     }
 }
