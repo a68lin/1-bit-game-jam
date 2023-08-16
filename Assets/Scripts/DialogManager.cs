7bfff75713c2ecb890364a03d1fdf21fb0d67d8e
Alex Lin <a68lin@uwaterloo.ca>
2023-08-16 00:09:58 -0400

    Add DialogManager

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,11 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour
 {
+    public Text message;
+    [Header("Remaining time")]
+    [Range(0, 20)]
+    private float timeToAppear = 5f;
+    private float timeWhenDisappear;
+
+    //private void Start()
+    //{
+    //    ShowDialog("Correct direction!");
+    //}
+
+    private void EnableText()
+    {
+        message.enabled = true;
+        timeWhenDisappear = Time.time + timeToAppear;
+    }
+
     public void ShowDialog(string text)
     {
+        message.text = text;
+        message.color = Color.black;
+        message.alignment = TextAnchor.UpperCenter;
+        message.fontSize = 18;
+        message.fontStyle = FontStyle.Bold;
+        EnableText();
+        RectTransform rt = message.GetComponent<RectTransform>();
+        rt.anchoredPosition3D = new Vector3(0, 70, 0);
+        rt.sizeDelta = new Vector2(1000, 50);
+    }
 
+    private void Update()
+    {
+        if (message.enabled && (Time.time >= timeWhenDisappear))
+        {
+            message.enabled = false;
+        }
     }
 }
