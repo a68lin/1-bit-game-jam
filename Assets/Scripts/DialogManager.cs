ac76acd1b0448ff70fdab3c142eb4cb3ec8175a6
Alex Lin <a68lin@uwaterloo.ca>
2023-08-17 00:01:10 -0400

    Add font prefab and final script

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class content
+{
+    public Text message;
+    public Image image;
+    public float timeWhenDisappear;
+    public float fadeTime;
+    public float appearTime;
+    public content(Text text, Image image, string info, float timeAppear, float timeFade)
+    {
+        this.message = text;
+        this.image = image;
+        this.timeWhenDisappear = Time.time + timeAppear;
+        this.fadeTime = timeFade;
+        this.appearTime = timeAppear;
+        this.message.text = info;
+        //this.message.color = Color.black;
+        //this.message.alignment = TextAnchor.UpperCenter;
+        //this.message.fontSize = 18;
+        //this.message.fontStyle = FontStyle.Bold;
+        //this.message.GetComponent<RectTransform>().anchoredPosition3D = position;
+        //this.message.GetComponent<RectTransform>().sizeDelta = new Vector2(1000, 50);
+    }
+}
+
 public class DialogManager : MonoBehaviour
 {
     public GameObject[] canvas;
     [Header("Appear time")]
     [Range(0, 10)]
-    public float appearTime = 3f;
+    public float appearTime;
     [Header("Fade time")]
     [Range(0, 10)]
-    public float fadeTime = 2f;
-    private Text message;
-    private float timeWhenDisappear = 5f;
-    //private const Vector3 position = new Vector3(0, 70, 0);
+    public float fadeTime;
+    private List<content> contents = new List<content>();
 
     private void Start()
     {
@@ -22,36 +44,55 @@ public class DialogManager : MonoBehaviour
 
     private void Update()
     {
-        if (message != null)
+        List<content> toRemove = new List<content>();
+        if (contents == null) return;
+        foreach (content dialog in contents)
         {
-            Color color = message.color;
-            if (color.a > 0 && Time.time >= timeWhenDisappear)
+            if (dialog.message != null)
             {
-                color.a -= Time.deltaTime / fadeTime;
-                message.color = color;
+                Color color = dialog.message.color;
+                if (color.a > 0 && Time.time >= dialog.timeWhenDisappear)
+                {
+                    color.a -= Time.deltaTime / dialog.fadeTime;
+                    dialog.message.color = color;
+
+                }
+                if (color.a > 0 && Time.time >= dialog.timeWhenDisappear)
+                {
+                    if (dialog.image != null)
+                    {
+                        color = dialog.image.color;
+                        color.a -= Time.deltaTime / dialog.fadeTime;
+                        dialog.image.color = color;
+                    }
+                }
             }
+            else
+            {
+                toRemove.Add(dialog);
+            }
+        }
+        foreach(content dialog in toRemove)
+        {
+            contents.Remove(dialog);
         }
     }
 
-    public void ShowDialog(string text, int index = 0, Vector3 position = default(Vector3), float timeAppear = 3f, float timeFade = 2f)
+    public void ShowDialog(string text, int index, float timeAppear, float timeFade)
     {
-        fadeTime = timeFade;
-        timeWhenDisappear = Time.time + timeAppear;
-
         GameObject copy = Instantiate<GameObject>(canvas[index], transform.position, Quaternion.identity);
 
-        message = copy.GetComponentInChildren<Text>();
-
-        message.text = text;
-        message.color = Color.black;
-        message.alignment = TextAnchor.UpperCenter;
-        message.fontSize = 18;
-        message.fontStyle = FontStyle.Bold;
-
-        RectTransform rt = message.GetComponent<RectTransform>();
-        rt.anchoredPosition3D = position;
-        rt.sizeDelta = new Vector2(1000, 50);
+        contents.Add(new content(copy.GetComponentInChildren<Text>(), copy.GetComponentInChildren<Image>(), text, timeAppear, timeFade));
 
         Destroy(copy, timeAppear + timeFade);
+    }
+
+    public void ShowDialog(string text, int index)
+    {
+        GameObject copy = Instantiate<GameObject>(canvas[index], transform.position, Quaternion.identity);
+
+        contents.Add(new content(copy.GetComponentInChildren<Text>(), copy.GetComponentInChildren<Image>(), text, appearTime, fadeTime));
+
+        Destroy(copy, appearTime + fadeTime);
     }
 }
